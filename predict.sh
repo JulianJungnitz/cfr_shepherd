31817ccadd81fd8aef22382328c277d71f4afa40
Julian Jungnitz <julian.jungnitz@web.de>
2024-11-23 17:37:13 +0100

    move all into one file

--- predict.sh
+++ predict.sh
@@ -1,3 +1,27 @@
+#!/bin/bash
+
+
+# remove old output files
+
+
+#SBATCH -J parprogprak1
+#SBATCH -e /work/scratch/kurse/kurs00084/tb89zyce/ba/cfr_shepherd/e.txt
+#SBATCH -o /work/scratch/kurse/kurs00084/tb89zyce/ba/cfr_shepherd/o.txt
+#SBATCH -C avx512
+#SBATCH -n 1
+#SBATCH --mem-per-cpu=1024
+#SBATCH --time=5
+#SBATCH --cpus-per-task=1
+#SBATCH -A kurs00084
+#SBATCH -p kurs00084
+#SBATCH --reservation=kurs00084
+
+
+
+echo "Job started"
+
+cd /work/scratch/kurse/kurs00084/tb89zyce/ba/cfr_shepherd
+
 cd SHEPHERD
 
 set -e
@@ -31,3 +55,7 @@ python predict.py \
     --node_map KG_node_map.txt          \
     --saved_node_embeddings_path checkpoints/pretrain.ckpt    \
     --best_ckpt checkpoints/patients_like_me.ckpt/patients_like_me.ckpt
+
+
+
+echo "Job finished"
--- sbatch_job.sh
+++ sbatch_job.sh
@@ -1,26 +1,3 @@
-#!/bin/bash
 
-
-# remove old output files
-
-
-#SBATCH -J parprogprak1
-#SBATCH -e /work/scratch/kurse/kurs00084/tb89zyce/ba/cfr_shepherd/e.txt
-#SBATCH -o /work/scratch/kurse/kurs00084/tb89zyce/ba/cfr_shepherd/o.txt
-#SBATCH -C avx512
-#SBATCH -n 1
-#SBATCH --mem-per-cpu=1024
-#SBATCH --time=5
-#SBATCH --cpus-per-task=1
-#SBATCH -A kurs00084
-#SBATCH -p kurs00084
-#SBATCH --reservation=kurs00084
-
-
-
-echo "Job started"
-
-cd /work/scratch/kurse/kurs00084/tb89zyce/ba/cfr_shepherd
 bash predict.sh
 
-echo "Job finished"
