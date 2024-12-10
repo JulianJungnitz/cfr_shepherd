dedd417dab2bdcef5a96782be6bdd6c523caf161
Julian Jungnitz <julian.jungnitz@web.de>
2024-12-10 18:46:11 +0100

    sbatch script anpassungen

--- predict_sbatch_job.sh
+++ predict_sbatch_job.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -A project02537
 #SBATCH -J cfr_shepherd
-#SBATCH -e /work/home/jj56rivo/cfr_shepherd/e_%j.txt
-#SBATCH -o /work/home/jj56rivo/cfr_shepherd/o_%j.txt
+#SBATCH -e /work/home/jj56rivo/cfr_shepherd/out/e_%j.txt
+#SBATCH -o /work/home/jj56rivo/cfr_shepherd/out/o_%j.txt
 #SBATCH -C avx512
 #SBATCH -n 1
 #SBATCH --mem-per-cpu=64G
