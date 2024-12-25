a8f0ad38536ee9a165552a1c36caa26a13749744
Julian Jungnitz <julian.jungnitz@web.de>
2024-12-25 11:37:40 +0100

    debug

--- app/SHEPHERD/shepherd/node_embedder_model.py
+++ app/SHEPHERD/shepherd/node_embedder_model.py
@@ -159,7 +159,9 @@ class NodeEmbeder(pl.LightningModule):
             # get negative targets by shuffling positive targets within each edge type
             et_ids, et_counts = all_edge_types.unique(return_counts=True)
             targets_dict = self.create_target_dict(all_edge_types, et_ids) # indices into all_edge_types for each edge type
+            print("Move to GPU: negative sampling")
             rand_index = torch.tensor(np.vectorize(sample_node_for_et)(all_edge_types.cpu(), targets_dict)).to(device)
+            print("Moved: negative sampling")
 
         if 'index_to_node_features_pos' in data:
             index_to_node_features_neg = data.index_to_node_features_pos[rand_index] #NOTE: currently possible to get the same node as positive & negative target
@@ -227,7 +229,9 @@ class NodeEmbeder(pl.LightningModule):
             tm = time.time()
             data = get_edges(data, self.all_data, dataset_type)
             te=time.time()
+        print("Move to GPU: _step - data")
         data = data.to(device)
+        print("Moved: _step - data")
 
         # Get predictions
         t0 = time.time()
@@ -370,7 +374,9 @@ class NodeEmbeder(pl.LightningModule):
         for i in range(len(self.convs)):
             
             # Update node embeddings
+            print("Move to GPU: predict - update node embeddings")
             x, (edge_i, alpha) = self.convs[i](x, data.edge_index.to(self.device), return_attention_weights=True) #
+            print("Moved: predict - update node embeddings")
 
             edge_i = edge_i.detach().cpu()
             alpha = alpha.detach().cpu()
