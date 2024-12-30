81296701a305215933be2826d49d339c5da58d17
Julian Jungnitz <julian.jungnitz@web.de>
2024-12-30 22:18:21 +0100

    updated install_pyg

---
+++ app/SHEPHERD/install_updated_pyg.sh
@@ -0,0 +1,5 @@
+pip install torch-scatter -f https://pytorch-geometric.com/whl/torch-2.4.1+cu121.html
+pip install torch-sparse -f https://pytorch-geometric.com/whl/torch-2.4.1+cu121.html
+pip install torch-cluster -f https://pytorch-geometric.com/whl/torch-2.4.1+cu121.html
+pip install torch-spline-conv -f https://pytorch-geometric.com/whl/torch-2.4.1+cu121.html
+pip install torch-geometric
