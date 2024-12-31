3597f408a588ac42adb7eba246bf5c85b1875ab9
Julian Jungnitz <julian.jungnitz@web.de>
2024-12-31 14:11:51 +0100

    test

--- app/SHEPHERD/shepherd/utils/pretrain_utils.py
+++ app/SHEPHERD/shepherd/utils/pretrain_utils.py
@@ -55,18 +55,14 @@ class HeterogeneousEdgeIndex(NamedTuple): #adopted from NeighborSampler code in 
 
 def get_batched_data(data, all_data):
     batch_size, n_id, adjs = data
-    print(f"edge_index: {all_data.edge_index.device}")
-    print(f"edge_attr: {all_data.edge_attr[adjs[0].e_id].device}")
-    print(f"adj: {adjs[0].device}")
-    adjs = [
-    HeterogeneousEdgeIndex(
-        adj.cpu(),
-        adj.e_id.cpu(),
-        all_data.edge_attr[adj.e_id.cpu()],
-        adj.size
-    )
-    for adj in adjs
-]
+    # print(f"edge_index: {all_data.edge_index.device}")
+    # print(f"edge_attr: {all_data.edge_attr[adjs[0].e_id].device}")
+    # print(f"adj: {adjs[0].device}")
+    
+    adjs = [HeterogeneousEdgeIndex(adj.edge_index, adj.e_id, all_data.edge_attr[adj.e_id], adj.size) for adj in adjs] 
+    
+
+
     data = Data(adjs = adjs, 
                 batch_size = batch_size,
                 n_id = n_id, 
