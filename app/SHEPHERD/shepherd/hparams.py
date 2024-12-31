efe783b30bbd3d407f14749bef41afa5fff91f4d
Julian Jungnitz <julian.jungnitz@web.de>
2024-12-31 19:16:50 +0100

    full training

--- app/SHEPHERD/shepherd/hparams.py
+++ app/SHEPHERD/shepherd/hparams.py
@@ -50,7 +50,7 @@ def get_pretrain_hparams(args, combined=False):
                
                'log_every_n_steps': 10,
                'time': False,
-               'debug': True
+               'debug': False
         }
 
     hparams = add_wandb_hparams(args, hparams)
