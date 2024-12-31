e42a8c1d8f1bd797e3dd947273bdaab5f04c324b
Julian Jungnitz <julian.jungnitz@web.de>
2024-12-31 11:31:17 +0100

    v3 creation

---
+++ v3_creation.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+#SBATCH -A project02537
+#SBATCH -J cfr_shepherd_install_v3
+#SBATCH --mail-user=julian.jungnitz@web.de
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH -e /work/home/jj56rivo/cfr_shepherd/out/e_%j.txt
+#SBATCH -o /work/home/jj56rivo/cfr_shepherd/out/o_%j.txt
+
+#SBATCH -n 1
+#SBATCH --mem-per-cpu=100G
+#SBATCH --time=00:30:00
+#SBATCH --cpus-per-task=1
+
+echo "Job started"
+
+conda env create -f v3_shepherd.yml
+
+conda env list
+
+echo "Job finished"
