150f23eed3bffb852d3785e632c7b49469eafec9
Julian Jungnitz <julian.jungnitz@web.de>
2024-12-31 14:07:58 +0100

    fix

--- app/SHEPHERD/shepherd/utils/pretrain_utils.py
+++ app/SHEPHERD/shepherd/utils/pretrain_utils.py
@@ -58,7 +58,7 @@ def get_batched_data(data, all_data):
     print(f"edge_index: {all_data.edge_index.device}")
     print(f"edge_attr: {all_data.edge_attr[adjs[0].e_id].device}")
     print(f"adj: {adjs[0].device}")
-    djs = [
+    adjs = [
     HeterogeneousEdgeIndex(
         adj.cpu(),
         adj.e_id.cpu(),
