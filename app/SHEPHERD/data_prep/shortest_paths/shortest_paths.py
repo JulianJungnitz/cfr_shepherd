e01b8812acc0a3c7e1afdbabff4828069075ef53
Julian Jungnitz <julian.jungnitz@web.de>
2025-01-03 15:48:05 +0100

    processes dynamic

--- app/SHEPHERD/data_prep/shortest_paths/shortest_paths.py
+++ app/SHEPHERD/data_prep/shortest_paths/shortest_paths.py
@@ -6,14 +6,15 @@ import snap
 import pandas as pd
 
 import sys
-#sys.path.insert(0, '..') # add config to path
-sys.path.insert(0, '../..') # add config to path
-sys.path.insert(0, '../../..') # add config to path
-sys.path.insert(0, '../../../..') # add config to path
+
+# sys.path.insert(0, '..') # add config to path
+sys.path.insert(0, "../..")  # add config to path
+sys.path.insert(0, "../../..")  # add config to path
+sys.path.insert(0, "../../../..")  # add config to path
 from app.SHEPHERD import project_config
 
 # Filenames
-suffix = "" # "_noGO" or ""
+suffix = ""  # "_noGO" or ""
 edgelist_f = "KG_edgelist_mask%s.txt" % suffix
 nodemap_f = "KG_node_map%s.txt" % suffix
 spl_mat_all_f = "KG_shortest_path_matrix%s.npy" % suffix
@@ -27,34 +28,48 @@ print(spl_mat_onlyphenotypes_f)
 
 print("Starting to calculate shortest paths...")
 
+processes = multiprocessing.cpu_count()
+print("Available processes:", processes)
+
 # INPUT KG FILE
 node_map = pd.read_csv(project_config.KG_DIR / nodemap_f, sep="\t")
-snap_graph = snap.LoadEdgeList(snap.PUNGraph, str(project_config.KG_DIR / edgelist_f), 0, 1)
+snap_graph = snap.LoadEdgeList(
+    snap.PUNGraph, str(project_config.KG_DIR / edgelist_f), 0, 1
+)
 
 t0 = time.time()
 
 node_ids = np.sort([node.GetId() for node in snap_graph.Nodes()])
 n_nodes = len(node_map)
 print(n_nodes, len(list(snap_graph.Nodes())), len(node_ids))
-print(f'There are {n_nodes} nodes in the graph')
+print(f"There are {n_nodes} nodes in the graph")
 assert max(node_ids) == n_nodes - 1
-if "noGO" not in edgelist_f: assert len(node_map) == len(node_ids)
+print("Node IDs are sorted and start from 0")
+if "noGO" not in edgelist_f:
+    assert len(node_map) == len(node_ids)
+print("Node map has the same number of nodes as the graph")
+
 
 def get_shortest_path(node_id):
+    print("Calculating shortest paths for node", node_id)
     NIdToDistH = snap.TIntH()
     path_len = snap.GetShortPath(snap_graph, int(node_id), NIdToDistH)
     paths = np.zeros((n_nodes))
-    for dest_node in NIdToDistH: 
+    for dest_node in NIdToDistH:
         paths[dest_node] = NIdToDistH[dest_node]
     return paths
 
-with multiprocessing.Pool(processes=20) as pool:
-    shortest_paths = pool.map(get_shortest_path, node_ids)
 
+# available processes
+
+with multiprocessing.Pool(processes=processes) as pool:
+    print("Starting multiprocessing")
+    shortest_paths = pool.map(get_shortest_path, node_ids)
+    print("Finished multiprocessing")
 all_shortest_paths = np.stack(shortest_paths)
 print(all_shortest_paths.shape)
 t1 = time.time()
-print(f'It took {t1-t0:0.4f}s to calculate the shortest paths')
+print(f"It took {t1-t0:0.4f}s to calculate the shortest paths")
 
 # save all shortest paths (requires no missing nodes)
 if "noGO" not in spl_mat_all_f:
@@ -65,5 +80,3 @@ desired_idx = node_map[node_map["node_type"] == "effect/phenotype"]["node_idx"].
 all_shortest_paths_to_phens = all_shortest_paths[:, desired_idx]
 with open(project_config.KG_DIR / spl_mat_onlyphenotypes_f, "wb") as f:
     np.save(f, all_shortest_paths_to_phens)
-
-
