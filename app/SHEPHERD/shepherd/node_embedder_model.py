9a1b16cb2a6a14a50135aa33926f25e5237152cb
Julian Jungnitz <julian@MacBook-Pro.local>
2025-01-18 21:13:40 +0100

    debug

--- app/SHEPHERD/shepherd/node_embedder_model.py
+++ app/SHEPHERD/shepherd/node_embedder_model.py
@@ -410,7 +410,9 @@ class NodeEmbeder(pl.LightningModule):
             
             # Update node embeddings
             print("Move to GPU: predict - update node embeddings")
-            x, (edge_i, alpha) = self.convs[i](x, data.edge_index.to(self.device), return_attention_weights=True) #
+            print(f"Memory allocated before layer {i}: {torch.cuda.memory_allocated(self.device)} bytes")
+            x, (edge_i, alpha) = self.convs[i](x, data.edge_index.to(self.device), return_attention_weights=True)
+            print(f"Memory allocated after layer {i}: {torch.cuda.memory_allocated(self.device)} bytes")
             print("Moved: predict - update node embeddings")
 
             edge_i = edge_i.detach().cpu()
