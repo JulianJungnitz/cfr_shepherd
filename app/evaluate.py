4f8e853b8825887737cf366d20058dba970de42e
Julian Jungnitz <julian@MacBook-Pro.local>
2025-01-22 14:33:49 +0100

    test

--- app/evaluate.py
+++ app/evaluate.py
@@ -196,7 +196,7 @@ if __name__ == "__main__":
     dir = project_config.PROJECT_DIR / "results" 
     file = dir / f"{base_res}_{agg_type}_primeKG_w_dis.csv"
     print(f"Evalute: {file}")
-    # # evaluate_patients_like_me(file)
+    evaluate_patients_like_me(file)
 
     disease_char_file = dir / "checkpoints.disease_characterization_scores_phen_primeKG_w_dis.csv"
     # evaluate_disease_characterization(disease_char_file)
