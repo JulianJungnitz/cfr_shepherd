c8912e4fb19da26bc10695c3db8137d4eb8ac1af
Julian Jungnitz <julian.jungnitz@web.de>
2025-01-29 21:40:19 +0100

    test

--- app/SHEPHERD/shepherd/node_embedder_model.py
+++ app/SHEPHERD/shepherd/node_embedder_model.py
@@ -410,18 +410,6 @@ class NodeEmbeder(pl.LightningModule):
 
     @torch.no_grad()
     def predict_subset(self, unique_node_ids, edge_index):
-        """
-        Predict embeddings for a subset of node IDs.
-
-        Args:
-            unique_node_ids (torch.Tensor): Tensor of unique node IDs in the batch.
-            edge_index (torch.Tensor): Edge indices corresponding to the subset (local indices).
-
-        Returns:
-            torch.Tensor: Node embeddings for the subset.
-            list: List of attention weights from each GAT layer.
-        """
-        # Move to device
         unique_node_ids = unique_node_ids.to(self.node_emb.weight.device)
         edge_index = edge_index.to(self.node_emb.weight.device)
 
@@ -432,25 +420,24 @@ class NodeEmbeder(pl.LightningModule):
         for i, conv in enumerate(self.convs):
             # Apply GAT layer
             x, (edge_i, alpha) = conv((x, x), edge_index, return_attention_weights=True)
-            # Store attention weights
             gat_attn.append((edge_i.detach().cpu(), alpha.detach().cpu()))
 
             # Apply normalization if necessary
             if self.norm_method in ["batch", "layer"]:
-                if self.norm_method == "batch" and self.norms:
-                    x = self.norms[i](x)
+                if self.norm_method == "batch" and self.batch_norms:
+                    x = self.batch_norms[i](x)
                 elif self.norm_method == "layer" and self.layer_norms:
                     x = self.layer_norms[i](x)
             elif self.norm_method == "batch_layer":
                 if self.layer_norms and self.batch_norms:
                     x = self.layer_norms[i](x)
-                    x = F.leaky_relu(x)
+                    x = nn.functional.leaky_relu(x)
                     x = self.batch_norms[i](x)
                 else:
-                    x = F.leaky_relu(x)
+                    x = nn.functional.leaky_relu(x)
 
             # Activation
-            x = F.leaky_relu(x)
+            x = nn.functional.leaky_relu(x)
 
         return x, gat_attn
 
