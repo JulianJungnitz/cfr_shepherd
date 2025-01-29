d2f2afe1e2195c68153827ed8c548f82a170dca3
Julian Jungnitz <julian.jungnitz@web.de>
2025-01-29 17:46:48 +0100

    test

---
+++ predict_sbatch_job.sh
@@ -0,0 +1,52 @@
+#!/bin/bash
+#SBATCH -A project02537
+#SBATCH -J cfr_shepherd
+#SBATCH --mail-user=julian.jungnitz@web.de
+#SBATCH --mail-type=BEGIN,END,FAIL
+#SBATCH -e /work/home/jj56rivo/cfr_shepherd/out/e_%j.txt
+#SBATCH -o /work/home/jj56rivo/cfr_shepherd/out/o_%j.txt
+
+#SBATCH -n 1
+#SBATCH --mem-per-cpu=200G
+#SBATCH --time=01:00:00
+#SBATCH --cpus-per-task=1
+
+
+
+echo "Job started"
+
+cd /work/home/jj56rivo/cfr_shepherd
+
+cd app/SHEPHERD
+
+set -e
+
+source /work/home/jj56rivo/miniconda3/etc/profile.d/conda.sh
+# source ~/anaconda3/etc/profile.d/conda.sh
+
+conda activate v5_shepherd
+# bash install_pyg.sh
+
+
+export PYTHONPATH="/home/jj56rivo/cfr_shepherd:$PYTHONPATH"
+export PYTORCH_CUDA_ALLOC_CONF="expandable_segments:True"
+
+
+conda activate v5_shepherd
+
+nvidia-smi
+
+echo "Memory usage before running predict.py:"
+free -h
+
+cd ../..
+# bash app/SHEPHERD/predict_patients_like_me.sh phenotypes
+# bash app/SHEPHERD/predict_causal_gene.sh
+bash app/SHEPHERD/predict_disease_categorization.sh phenotypes _hauner hauner
+# python app/test_shepherd.py
+
+echo "Memory usage after running predict.py:"
+free -h
+
+
+echo "Job finished"
