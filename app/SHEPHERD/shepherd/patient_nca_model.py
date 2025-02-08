b69dad54f94a57e4c3957c7504191071c5224b81
Julian Jungnitz <julian.jungnitz@web.de>
2025-02-08 13:29:52 +0100

    test

--- app/SHEPHERD/shepherd/patient_nca_model.py
+++ app/SHEPHERD/shepherd/patient_nca_model.py
@@ -398,9 +398,9 @@ class CombinedPatientNCA(pl.LightningModule):
         print("Forward:")
         # node_embeddings, gat_attn, phenotype_embedding, disease_embeddings, phenotype_mask, disease_mask, attn_weights = self.forward(batch)
         
-        # correct_ranks, softmax, labels, node_embedder_loss, patient_loss, roc_score, ap_score, acc, f1, gat_attn, node_embeddings, phenotype_embedding, disease_embeddings, phenotype_mask, disease_mask, attn_weights, cand_disease_idx, cand_disease_embeddings  = self._step(batch, 'test')
+        correct_ranks, softmax, labels, node_embedder_loss, patient_loss, roc_score, ap_score, acc, f1, gat_attn, node_embeddings, phenotype_embedding, disease_embeddings, phenotype_mask, disease_mask, attn_weights, cand_disease_idx, cand_disease_embeddings  = self._step(batch, 'test')
 
-        node_embeddings, gat_attn, phenotype_embedding, disease_embeddings, phenotype_mask, disease_mask, attn_weights = self.inference(batch, batch_idx)
+        # node_embeddings, gat_attn, phenotype_embedding, disease_embeddings, phenotype_mask, disease_mask, attn_weights = self.inference(batch, batch_idx)
 
         # calculate patient embedding loss
         use_candidate_list =  self.hparams.hparams['only_hard_distractors'] 
