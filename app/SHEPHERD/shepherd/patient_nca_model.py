1574876fb7492c0a1440d2dd458ca52aac40c99c
Julian Jungnitz <julian.jungnitz@web.de>
2025-02-08 20:24:01 +0100

    test

--- app/SHEPHERD/shepherd/patient_nca_model.py
+++ app/SHEPHERD/shepherd/patient_nca_model.py
@@ -175,7 +175,7 @@ class CombinedPatientNCA(pl.LightningModule):
         return batch_results
 
     def validation_step(self, batch, batch_idx):
-        pass
+        return
         correct_ranks, softmax, labels, node_embedder_loss, patient_loss, roc_score, ap_score, acc, f1, gat_attn, node_embeddings, phenotype_embedding, disease_embeddings, phenotype_mask, disease_mask, attn_weights, cand_disease_idx, cand_disease_embeddings = self._step(batch, 'val')
         print("Patient loss: ", patient_loss)
         print("Node embedder loss: ", node_embedder_loss)
