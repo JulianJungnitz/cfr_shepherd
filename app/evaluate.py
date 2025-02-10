8eeac15bf2bfd6855b128dca705c49c073e6d792
Julian Jungnitz <julian.jungnitz@web.de>
2025-02-10 11:25:53 +0100

    test

--- app/evaluate.py
+++ app/evaluate.py
@@ -370,9 +370,11 @@ def evaluate_disease_characterization(
     result = utils.execute_query(driver, query)
     db_diseases = set([record["disease_id"] for record in result])
 
-
+    print("Different diseases in db: ", len(db_diseases))
+    print("First 5 diseases in db: ", {k: v for k, v in list(db_diseases)[:5]})
     # df = map_disease_to_doid(df)
     df["doid_full"] = df["diseases"]
+    print("First 5 diseases in df: ", df["doid_full"].unique()[:5])
     # filter for diseases in db
     print("length of df: " + str(len(df)))
     df = df[df["doid_full"].isin(db_diseases)]
