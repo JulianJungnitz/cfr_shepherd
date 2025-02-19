c5690f1326540f540e8a52e4a3be16d612066405
Julian Jungnitz <julian@MacBook-Pro.local>
2025-02-19 16:44:38 +0100

    test

--- app/evaluate.py
+++ app/evaluate.py
@@ -363,8 +363,8 @@ def evaluate_disease_characterization(
     file_name,
 ):
     df = pd.read_csv(file_name)
-    get_ideal_threshold(df)
-    return
+    # get_ideal_threshold(df)
+    # return
     driver = utils.connect_to_neo4j()
     query = 'Match (d:Disease)<-[:HAS_DISEASE]-(b:Biological_sample) return d.id as disease_id'
     # result = utils.execute_query(driver, query)
@@ -418,8 +418,8 @@ def evaluate_disease_characterization(
     
     print("number of patients: " + str(number_of_patients))
 
-    threshold = 0.00007343292236328125
-    # Filter rows with similarities above the threshold
+    threshold = 0.00014400482177734
+    
     filtered_df = df[df["similarities"] > threshold]
     print("length of filtered df: " + str(len(filtered_df)))
     print("Filterded head: ", filtered_df.head())
